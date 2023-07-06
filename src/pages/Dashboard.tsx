5f0a55cb4acb0e6277cea72410a0432b58533230
Jordan <jordan.su@protonmail.com>
2023-07-05 21:01:43 -0400

    CU-860r8xupz[in progress] - update upload and image results page styles

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -6,8 +6,22 @@ const Dashboard: React.FC = () => {
     return (
         <MainLayout>
             <AuthLayout>
-                <h1>Dashboard</h1>
-                <p>Welcome back</p>
+                <div className='flex min-h-full flex-1 flex-col justify-center px-6 lg:px-8'>
+                    <div className='mx-auto max-w-2xl'>
+                        <header className='bg-white shadow'>
+                            <div className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>
+                                <h1 className='text-3xl font-bold tracking-tight text-gray-900'>
+                                    Dashboard
+                                </h1>
+                            </div>
+                        </header>
+                        <p className='mt-6 text-lg leading-8 text-gray-600'>
+                            Anim aute id magna aliqua ad ad non deserunt sunt.
+                            Qui irure qui lorem cupidatat commodo. Elit sunt
+                            amet fugiat veniam occaecat fugiat aliqua.
+                        </p>
+                    </div>
+                </div>
             </AuthLayout>
         </MainLayout>
     );
--- src/pages/Images.tsx
+++ src/pages/Images.tsx
@@ -7,15 +7,26 @@ const ImagesPage: React.FC = () => {
     return (
         <MainLayout>
             <AuthLayout>
-                <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
-                    <div className='mx-auto max-w-2xl py-12 sm:py-48 lg:py-12'>
-                        <h1 className='mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
-                            Processed Images
-                        </h1>
+                <div className='flex min-h-full flex-1 flex-col justify-center px-6 lg:px-8'>
+                    <div className='mx-auto max-w-2xl'>
+                        <header className='bg-white shadow'>
+                            <div className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>
+                                <h1 className='text-3xl font-bold tracking-tight text-gray-900'>
+                                    Processed Images
+                                </h1>
+                            </div>
+                        </header>
                         <p className='mt-6 text-lg leading-8 text-gray-600'>
-                            Anim aute id magna aliqua ad ad non deserunt sunt.
-                            Qui irure qui lorem cupidatat commodo. Elit sunt
-                            amet fugiat veniam occaecat fugiat aliqua.
+                            Welcome to the processed images page! Here, you can
+                            view the results of your uploaded images that have
+                            been processed using the powerful AWS Rekognition
+                            service. Each image has undergone comprehensive
+                            analysis, allowing us to detect and identify various
+                            elements within the image. Gain valuable insights
+                            into the contents of your images and explore the
+                            detailed labels assigned to them. Take a closer look
+                            at the results and delve into the fascinating world
+                            of image recognition.
                         </p>
                     </div>
                     <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
