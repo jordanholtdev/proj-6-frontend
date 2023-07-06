4092074a478e3b52da861bd222ce20139a0f3406
Jordan <jordan.su@protonmail.com>
2023-07-05 20:46:58 -0400

    CU-860r8xupz[in progress] - update home page to include redirects

--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -1,17 +1,17 @@
 // navigation component
-import React, { Fragment } from 'react';
+import React from 'react';
 import AuthStatus from '../auth/AuthStatus';
 import { Link } from 'react-router-dom';
-import { Disclosure, Menu, Transition } from '@headlessui/react';
-import {
-    Bars3Icon,
-    XMarkIcon,
-    ArrowDownLeftIcon,
-    CpuChipIcon,
-} from '@heroicons/react/24/outline';
+import { Disclosure } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon, CpuChipIcon } from '@heroicons/react/24/outline';
 
 const Nav: React.FC = () => {
     const navItems = [
+        {
+            id: 1,
+            name: 'Home',
+            path: '/',
+        },
         {
             id: 2,
             name: 'Dashboard',
@@ -80,40 +80,11 @@ const Nav: React.FC = () => {
                                                 {item.name}
                                             </Link>
                                         ))}
+                                        <AuthStatus />
                                     </div>
                                 </div>
                             </div>
-                            <div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
-                                {/* Logout dropdown */}
-                                <Menu as='div' className='relative ml-3'>
-                                    <div>
-                                        <Menu.Button className='flex rounded-full text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'>
-                                            <span className='sr-only'>
-                                                Open user menu
-                                            </span>
-                                            <ArrowDownLeftIcon
-                                                className='block h-6 w-6'
-                                                aria-hidden='true'
-                                            />
-                                        </Menu.Button>
-                                    </div>
-                                    <Transition
-                                        as={Fragment}
-                                        enter='transition ease-out duration-100'
-                                        enterFrom='transform opacity-0 scale-95'
-                                        enterTo='transform opacity-100 scale-100'
-                                        leave='transition ease-in duration-75'
-                                        leaveFrom='transform opacity-100 scale-100'
-                                        leaveTo='transform opacity-0 scale-95'
-                                    >
-                                        <Menu.Items className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
-                                            <Menu.Item>
-                                                <AuthStatus />
-                                            </Menu.Item>
-                                        </Menu.Items>
-                                    </Transition>
-                                </Menu>
-                            </div>
+                            <div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'></div>
                         </div>
                     </div>
 
@@ -131,6 +102,7 @@ const Nav: React.FC = () => {
                                     {item.name}
                                 </Disclosure.Button>
                             ))}
+                            <AuthStatus />
                         </div>
                     </Disclosure.Panel>
                 </>
